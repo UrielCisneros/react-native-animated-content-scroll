7112aaf4818462578074a4532593c92353383e7d
Uriel Cisneros <urielcisne96@gmail.com>
2025-06-17 18:16:03 -0600

    add example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,20 +1,16 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from 'react-native-animated-content-scroll';
-
-const result = multiply(3, 7);
+import { AnimatedContentScroll } from 'react-native-animated-content-scroll';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
+    <View style={{ padding: 20 }}>
+      {[...Array(5)].map((_, index) => (
+        <AnimatedContentScroll key={index} index={index}>
+          <Text style={{ fontSize: 18, marginBottom: 10 }}>
+            Item {index + 1}
+          </Text>
+        </AnimatedContentScroll>
+      ))}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
